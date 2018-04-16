0b91d47c4b04142dce4b1198fe64640cf4fa68a9
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-04-16 18:35:51 +0800

    OK

--- chat_app.js
+++ chat_app.js
@@ -1,5 +1,28 @@
 "use strict";
 {
 
+const p = new SimplePeer({initiator: location.hash === '#1', trickle: false });
 
+  p.on('data', y => console.log("Data", y));
+
+p.on('error', function (err) { console.log('error', err) })
+
+  p.on('signal', function (data) {
+    console.log('SIGNAL', JSON.stringify(data))
+    document.querySelector('input[name="comment"]').value = JSON.stringify(data);
+  })
+
+  document.querySelector('form').addEventListener('submit', function (ev) {
+    ev.preventDefault()
+    p.signal(document.querySelector('input[name="comment"]').value)
+  })
+
+  p.on('connect', function () {
+    console.log('CONNECT')
+    p.send('whatever' + Math.random())
+  })
+
+  p.on('data', function (data) {
+    console.log('data: ' + data)
+  })
 }
