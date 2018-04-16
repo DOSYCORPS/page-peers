37f0108051477b2745c2493dbe71d1fabbc7aa99
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-04-16 16:18:10 +0800

    OK

--- run_bg.js
+++ run_bg.js
@@ -1,7 +1,5 @@
 "use strict";
 {
-  chrome.tabs.create({active:true,url: chrome.extension.getURL('ui.html')});
-
   chrome.runtime.onMessage.addListener( (msg, sender, reply) => {
     console.log("MSG", msg);
   });
